b181ce35e98e1bde799f634a54760c1b26e5f46a
rmukhammadiev <rmukhammadiev@igsystems.ru>
2013-07-26 17:27:42 +0400

    ++

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+/*Сортировка элементов*/
+$(document).ready(function(){
+		$(".radio").click(function() {
+		$(".triangle").hide();			
+		$(this).find('.triangle').show();
+		// if ($('.triangle').hasClass('triangle_down')){
+				// $(this).find('.triangle').removeClass('triangle_down');
+				// $(this).find('.triangle').addClass('triangle_up');
+			// }
+		// else {
+				// $(this).find('.triangle').removeClass('triangle_up');
+				// $(this).find('.triangle').addClass('triangle_down');
+			// };
+		$(".triangle").toggleClass("triangle_down triangle_up");
+		$(".radio_select").removeClass("active");
+		$(this).find(".radio_select").addClass("active");
+		
+	});
+});
+
 
